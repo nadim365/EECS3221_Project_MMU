cb7eef71c6df86438a0d5925b4388afb19bb9c84
nadim <samnad14@gmail.com>
2021-12-05 23:12:35 -0500

    added definitons for adding and searching TLB

--- StartKit/func.c
+++ StartKit/func.c
@@ -1,11 +1,22 @@
 #include "func.h"
 #include "tlb.h"
 
-int search_pg(unsigned char log_pg, int index){
+int search_pg(unsigned char log_pg, int index, struct TLB_val x[]){
     int i;
     for (i = max((index - 16), 0); i < index; i++)
     {
-        /* code */
+        struct TLB_val *page = &x[i & 16];
+        if (page->log_add == log_pg)
+        {
+            return page->phy_add;
+        }
+        
     }
-    
+    return -1;
+}
+
+void add_pg(unsigned char log_pg, unsigned char phy_pg, struct TLB_val x[], int index){
+    struct TLB_val *page = &x[index % 16]
+    page->log_add = log_pg;
+    page->phy_add = phy_pg;    
 }
--- StartKit/func.h
+++ StartKit/func.h
@@ -1,4 +1,4 @@
 #include "tlb.h"
 
-int search_pg(unsigned char log_pg, int index);
-int add_pg(unsigned char log_pg, unsigned char phy_pg);
+int search_pg(unsigned char log_pg, int index, struct TLB_val x[]);
+void add_pg(unsigned char log_pg, unsigned char phy_pg, struct TLB_val x[], int index);
