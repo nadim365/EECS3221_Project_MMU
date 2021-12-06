97fc7cedc721f92f117bdb095a7d59ce560ebe7a
nadim <samnad14@gmail.com>
2021-12-05 21:39:47 -0500

    added files containing functions to be used by TLB

---
+++ StartKit/func.c
@@ -0,0 +1,11 @@
+#include "func.h"
+#include "tlb.h"
+
+int search_pg(unsigned char log_pg, int index){
+    int i;
+    for (i = max((index - 16), 0); i < index; i++)
+    {
+        /* code */
+    }
+    
+}
---
+++ StartKit/func.h
@@ -0,0 +1,4 @@
+#include "tlb.h"
+
+int search_pg(unsigned char log_pg, int index);
+int add_pg(unsigned char log_pg, unsigned char phy_pg);
